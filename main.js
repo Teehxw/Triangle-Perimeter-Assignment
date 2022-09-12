843343bdd7535299607bb4844211fe368a0ca214
Teehxw <112659668+Teehxw@users.noreply.github.com>
2022-09-12 14:47:19 -0600

    done

--- main.js
+++ main.js
@@ -2,12 +2,6 @@
 
 //Variables to store HTML elements
 let calcBtn = document.getElementById("calc");
-let lengthxA = +document.getElementById("xA").value;
-let lengthyA = +document.getElementById("yA").value;
-let lengthxB = +document.getElementById("xB").value;
-let lengthyB = +document.getElementById("yB").value;
-let lengthxC = +document.getElementById("xC").value;
-let lengthyC = +document.getElementById("yB").value;
 let finalP = document.getElementById("sum");
 let ABresultE1 = document.getElementById("sumAB");
 let ACresultE1 = document.getElementById("sumAC");
@@ -19,13 +13,31 @@ calcBtn.addEventListener("click", calcPerimeter);
 
 //Function to Calculate perimeter 
 function calcPerimeter (){
-//Process
-
-let final= lengthxA + lengthyA;
-finalP.innerHTML = final ; 
-
+//Input
+let lengthxA = +document.getElementById("xA").value;
+let lengthyA = +document.getElementById("yA").value;
+let lengthxB = +document.getElementById("xB").value;
+let lengthyB = +document.getElementById("yB").value;
+let lengthxC = +document.getElementById("xC").value;
+let lengthyC = +document.getElementById("yC").value;
+    //Process
+// let AB=  lengthxA + lengthyA + lengthxB + lengthyB;
+// ABresultE1.innerHTML = AB ; 
+// let AC=  lengthxA + lengthyA + lengthxC + lengthyC;
+// ACresultE1.innerHTML = AC ;
+// let BC=  lengthxB + lengthyB + lengthxC + lengthyC;
+// BCresultE1.innerHTML = BC ;
+let AB = Math.sqrt((lengthxB-lengthxA)*(lengthxB-lengthxA) + (lengthyB-lengthyA)*(lengthyB-lengthyA));
+let AC =  Math.sqrt((lengthxC-lengthxA)*(lengthxC-lengthxA) + (lengthyC-lengthyA)*(lengthyC-lengthyA));
+let BC =  Math.sqrt((lengthxC-lengthxB)*(lengthxC-lengthxB) + (lengthyC-lengthyB)*(lengthyC-lengthyB));
+let perimeter = AB + AC + BC;
+//Output
+ABresultE1.innerHTML = AB;
+ACresultE1.innerHTML = AC;
+BCresultE1.innerHTML = BC;
+finalP.innerHTML = perimeter;
 }
 
-// function dist(x1value, y1value, x2value, y2value){
-
-// }
+//function dist(x1value, y1value, x2value, y2value){
+//Math.sqrt((x2value-x1value)*(x2value-x1value) + (y2value-y1value)*(y2value-y1value));
+//}
